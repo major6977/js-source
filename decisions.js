02e952a8b43ad7695141e290b172805cc8e84489
major davis <majordav@msn.com>
2022-01-26 16:22:03 -0700

    added decisions,function-declaration and expression-declaration

---
+++ decisions.js
@@ -0,0 +1,6 @@
+var count = 3 ;
+if (count == 4) {
+    
+    console.log('count is 4');
+
+}
---
+++ function-expressions.js
@@ -0,0 +1,10 @@
+// setTimeout(function () {
+//   console.log(" I want 2 seconds");
+// }, 2000); 
+
+// l
+
+(function() {
+    console.log('immediatley Invoked Function Expression (IIFE)');
+
+})();
