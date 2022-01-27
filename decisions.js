83773b738032e655c24e911994b529e2b1f35522
major davis <majordav@msn.com>
2022-01-27 14:20:11 -0700

    added iterations,scope and modified decisions.js

--- decisions.js
+++ decisions.js
@@ -1,6 +1,25 @@
-var count = 3 ;
-if (count == 4) {
-    
-    console.log('count is 4');
+// var count = '4' ;
+// if (count === 4) {
+//     console.log("it's a go");
+// } else {
+//     console.log("Hard Stop");
+// }
 
-}
+// let hero = 'Blankman';
+// switch (hero) {
+//     case 'Blankman':
+//         console.log('can fly very fast');
+//         console.log('ex-ray vision');
+//         break;
+//     case 'Ant-Man':
+//         console.log('very tiny');
+//         console.log('super strong');
+//         break;
+//     default:
+//         console.log('Super Hero Of Today');  
+// } 
+
+// Ternary operator
+
+let a = 2, b ='4';
+console.log(a==b ? 'go for it' : 'fire Terminate');
---
+++ iterations.js
@@ -0,0 +1,9 @@
+let a = [4, 8, 15, 16, 23, 42];
+for (i = 0; i < a.length; i++) {
+        console.log(a[i]);
+    }
+    
+
+    
+
+    
